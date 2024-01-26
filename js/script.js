418f6143835c76820d519d0e7b62afc58c8b3d70
Blaze <111865477+geGDVS@users.noreply.github.com>
2024-01-26 16:08:52 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // 打开一个 web socket  这里端口号和上面监听的需一致
-var ws = new WebSocket('wss://pnode1.danbot.host:7445/');
+var ws = new WebSocket('ws://pnode1.danbot.host:7445/');
 var messageDiv = document.getElementById("m");
 var input = document.getElementById("text");
 
