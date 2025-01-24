77ab6009d3538eb7604c314dd87de927193c41b3
Blaze <111865477+geGDVS@users.noreply.github.com>
2025-01-24 22:31:31 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,86 @@
-// 打开一个 web socket  这里端口号和上面监听的需一致
-var ws = new WebSocket('ws://pnode1.danbot.host:7445/');
-var messageDiv = document.getElementById("m");
-var input = document.getElementById("text");
-
-// Web Socket 已连接上，使用 send() 方法发送数据
-ws.onopen = function() {
-	ws.send('Client joiend.');
+lounge = document.getElementById("lounge")
+text = document.getElementById("text")
+lounge.onclick = function() {
+	lounge.active = true;
+	text.label = "Enter Name#Password to join the channel";
+	channel = 'lounge';
 }
-// 这里接受服务器端发过来的消息
-ws.onmessage = function(e, isBinary) {
-	messageDiv.innerHTML += `<p>${e.data}</p>`;
+text.onkeydown = function (e) {
+	if (e.keyCode == 13 /* ENTER */ && !e.shiftKey) {
+		e.preventDefault();
+
+		// Submit message
+		if (e.target.value != '') {
+			var text = e.target.value;
+			e.target.value = '';
+			
+			if (channel != "in"){
+				ws_send({ cmd: 'join', nick: text, channel:channel});
+				channel = "in";
+				text.label = "Enter Text to chat"
+				text.clearable = true;
+			} else {
+				ws_send({ cmd: 'chat', text: text });
+			}
+		}
+	}
 }
 
-function send(){
-	ws.send(input.value);
+var ws = new WebSocket("wss://hack.chat/chat-ws");
+function ws_send(data) {
+    if (ws && ws.readyState == ws.OPEN) {
+        ws.send(JSON.stringify(data));
+    }
+}
+ws.onmessage = function(message) {
+    var result = JSON.parse(message.data);
+    var cmd = result.cmd;
+    var command = COMMANDS[cmd];
+    if (command) {
+        command.call(null, result);
+    }
+}
+COMMANDS = {
+    "info": function(args) {
+        args.nick = "*";
+        pushMessage(args);
+    },
+    "emote": function(args) {
+        args.nick = "*";
+        pushMessage(args);
+    },
+    "chat": function(args) {
+        pushMessage(args);
+    },
+    "onlineAdd": function(args) {
+        args.text = args.nick + " joined";
+        args.nick = "*";
+        pushMessage(args);
+    },
+    "onlineRemove": function(args) {
+        args.text = args.nick + " left";
+        args.nick = "*";
+        pushMessage(args);
+    },
+    "onlineSet": function(args) {
+        args.nick = "*";
+        args.text = "Users online: " + args.nicks.join(", ");
+        pushMessage(args);
+    },
+    "warn": function(args) {
+        args.nick = "!";
+        args.text = args.text;
+        pushMessage(args);
+    },
+}
+function pushMessage(args) {
+    $('#main').innerHTML += `
+    <mdui-card variant="outlined" style="width: 300px">
+		<mdui-chip>${args.nick}</mdui-chip>
+		<div class="mdui-prose">
+			${args.text}
+		</div>
+	</mdui-card>
+    `;
+    window.scrollTo(0, document.body.scrollHeight);
 }
